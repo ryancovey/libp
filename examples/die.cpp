96ee02f392ad1b83b89843efeadb407fc2254b0b
Ryan Zischke <ryan@melb.zischke.net.au>
2021-08-24 18:08:46 +1000

    The FiniteSet class can now interact with the UniversalSet, and sets of All<T> type. Another item to do soon is an example on the sets themselves. Right now set operations are taking up the majority of the code.

--- examples/die.cpp
+++ examples/die.cpp
@@ -2,37 +2,71 @@
 #include <libp/libp.hpp>
 
 using namespace libp;
+using std::cout;
+using std::endl;
 
 int main(void) {
-    // auto P = UniformDistribution<FiniteSet<int>>({1,2,3,4,5,6});
+    // We can obtain an object representing the distribution of a standard six sided dice as follows.
     auto P = uniform_distribution({1,2,3,4,5,6});
+    cout << "P = uniform_distribution({1,2,3,4,5,6})" << endl;
+    cout << endl;
 
-    // We can asign events to variables...
-    FiniteSet<int> A = {1,2,3};
-    FiniteSet<int> B = {3,4,5,6};
-    std::cout << "A = {1,2,3}" << std::endl;
-    std::cout << "B = {3,4,5,6}" << std::endl;
-    std::cout << "P(A) == " << P(A) << std::endl;
-    std::cout << "P(B) == " << P(B) << std::endl;
-    std::cout << "P(A && B) == " << P(A && B) << std::endl;
-    std::cout << "P(A || B) == " << P(A || B) << std::endl;
-    std::cout << "P(A|B) == " << P(A|B) << std::endl;
-    std::cout << "P(B|A) == " << P(B|A) << std::endl;
+    // If we want to specify the time of the elements underlying the distribution, to be unsigned
+    // for example, we can write the following.
+    // auto P = UniformDistribution(finite_set<unsigned>({1,2,3,4,5,6}));
+
+    // We can assign events to variables...
+
+    auto A = finite_set({1,2,3});
+    auto B = finite_set({3,4,5,6});
+    cout << "A = {1,2,3}" << endl;
+    cout << "B = {3,4,5,6}" << endl;
+    cout << endl;
+
+    cout << "P(A) == " << P(A) << endl;
+    cout << "P(B) == " << P(B) << endl;
+    cout << "P(A && B) == " << P(A && B) << endl;
+    cout << "P(B && A) == " << P(B && A) << endl;
+    cout << "P(A || B) == " << P(A || B) << endl;
+    cout << "P(B || A) == " << P(B || A) << endl;
+    cout << "P(A|B) == " << P(A|B) << endl;
+    cout << "P(B|A) == " << P(B|A) << endl;
+    cout << endl;
 
     // ...or define them inline...
-    std::cout << "P({1,2,3}) == " << P({1,2,3}) << std::endl;
-    std::cout << "P({3,4,5,6}) == " << P({3,4,5,6}) << std::endl;
+    
+    cout << "P({1,2,3}) == " << P({1,2,3}) << endl;
+    cout << "P({3,4,5,6}) == " << P({3,4,5,6}) << endl;
+    cout << endl;
+    
     // ... but not always! C++ infix operators such as "&&", "||", and "|" cannot be used with braced arguments.
-    // std::cout << "P({1,2,3} && {3,4,5,6}) == " << P({1,2,3} && {3,4,5,6}) << std::endl;
-    // std::cout << "P({1,2,3} || {3,4,5,6}) == " << P({1,2,3} || {3,4,5,6}) << std::endl;
-    // std::cout << "P({1,2,3}|{3,4,5,6}) == " << P({1,2,3}|{3,4,5,6}) << std::endl;
-    // std::cout << "P({3,4,5,6}|{1,2,3}) == " << P({3,4,5,6}|{1,2,3}) << std::endl;
+    
+    // cout << "P({1,2,3} && {3,4,5,6}) == " << P({1,2,3} && {3,4,5,6}) << endl;
+    // cout << "P({1,2,3} || {3,4,5,6}) == " << P({1,2,3} || {3,4,5,6}) << endl;
+    // cout << "P({1,2,3}|{3,4,5,6}) == " << P({1,2,3}|{3,4,5,6}) << endl;
+    // cout << "P({3,4,5,6}|{1,2,3}) == " << P({3,4,5,6}|{1,2,3}) << endl;
+    
     // But we can wrap our inline sets in the finite_set function for the same effect.
-    std::cout << "P(finite_set({1,2,3}) && finite_set({3,4,5,6})) == " << P(finite_set({1,2,3}) && finite_set({3,4,5,6})) << std::endl;
-    std::cout << "P(finite_set({1,2,3}) || finite_set({3,4,5,6})) == " << P(finite_set({1,2,3}) || finite_set({3,4,5,6})) << std::endl;
-    std::cout << "P(finite_set({1,2,3})|finite_set({3,4,5,6})) == " << P(finite_set({1,2,3})|finite_set({3,4,5,6})) << std::endl;
-    std::cout << "P(finite_set({3,4,5,6})|finite_set({1,2,3})) == " << P(finite_set({3,4,5,6})|finite_set({1,2,3})) << std::endl;
+    
+    cout << "P(finite_set({1,2,3}) && finite_set({3,4,5,6})) == " << P(finite_set({1,2,3}) && finite_set({3,4,5,6})) << endl;
+    cout << "P(finite_set({1,2,3}) || finite_set({3,4,5,6})) == " << P(finite_set({1,2,3}) || finite_set({3,4,5,6})) << endl;
+    cout << "P(finite_set({1,2,3})|finite_set({3,4,5,6})) == " << P(finite_set({1,2,3})|finite_set({3,4,5,6})) << endl;
+    cout << "P(finite_set({3,4,5,6})|finite_set({1,2,3})) == " << P(finite_set({3,4,5,6})|finite_set({1,2,3})) << endl;
+    cout << endl;
 
+    // We can also mix and match the types of elements in the sets. In general, two elements of different types are equal
+    // if and only if converting from one type to the other and back again does not change the elements.
+    
+    auto Bd = finite_set({3.0,4.0,5.0,6.0,7.5});
+    cout << "P(Bd) == " << P(Bd) << endl;
+    cout << "P(A && Bd) == " << P(A && Bd) << endl;
+    cout << "P(Bd && A) == " << P(Bd && A) << endl;
+    cout << "P(A || Bd) == " << P(A || Bd) << endl;
+    cout << "P(Bd || A) == " << P(Bd || A) << endl;
+    cout << "P(A|Bd) == " << P(A|Bd) << endl;
+    cout << "P(Bd|A) == " << P(Bd|A) << endl;
+    cout << endl;
+    
     return 0;
 }
 
--- include/libp/sets/finite.hpp
+++ include/libp/sets/finite.hpp
@@ -8,8 +8,11 @@
 #include <utility>
 #include <vector>
 #include <set>
+#include <libp/internal/constants.hpp>
+#include <libp/sets/all.hpp>
 #include <libp/sets/conditional.hpp>
 #include <libp/sets/measurable_set.hpp>
+#include <libp/sets/none.hpp>
 
 namespace libp {
 
@@ -46,9 +49,174 @@ namespace libp {
                 make_sorted_unique(storage);
             }
 
+            // If a MeasurableSet of objects not of a similar type to T participates in an intersection
+            // with this FiniteSet, clearly none of the elements represented in the former MeasurableSet
+            // can be present in the latter.
+            template<
+                class R,
+                std::enable_if_t<std::is_base_of<MeasurableSet, std::decay_t<R>>::value, bool> = true
+            >
+            auto operator&&(const R&) const {
+                return none();
+            }
+
+            // We have not yet implemented a set that can represent the union of
+            // sets with elements of distinct types.
+            /*
+            template<class R>
+            auto operator||(const R&) const {
+                // todo.
+            }
+            */
+
+            auto operator&&(const UniversalSet&) const {
+                return *this;
+            }
+
+            auto operator||(const UniversalSet&) const {
+                return all();
+            }
+
+            // If objects of type T cannot be converted to RT and back, then surely
+            // *this && all<RT>() == none().
+            template<
+                class RT,
+                std::enable_if_t<
+                    (!std::is_convertible<T, RT>::value || !std::is_convertible<RT, T>::value) &&
+                    (!std::is_arithmetic<T>::value || !std::is_arithmetic<RT>::value),
+                    bool
+                > = true
+            >
+            auto operator&&(const All<RT>&) {
+                return none();
+            }
+
+            // If *this is a subset of all<RT>(), *this && all<RT>() == *this. Right now we're only doing
+            // this for standard arithmetic types.
+            template<
+                class RT,
+                std::enable_if_t<
+                    (std::is_arithmetic<T>::value && std::is_floating_point<RT>::value) ||  // RT are the reals
+                    (
+                        std::is_integral<T>::value && !std::is_signed<T>::value &&          // T and RT are the integers.
+                        std::is_integral<RT>::value && !std::is_signed<RT>::value
+                    ) ||
+                    (std::is_signed<T>::value && std::is_arithmetic<RT>::value),            // T are the naturals and zero.
+                    bool
+                > = true
+            >
+            auto operator&&(const All<RT>&) {
+                return *this;
+            }
+
+            // If *this is a subset of the integers and all<RT>() contains the naturals and zero,
+            // then *this && all<RT>() contain all elements of *this that are >= 0.
+            template<
+                class RT,
+                std::enable_if_t<
+                    std::is_integral<T>::value && std::is_unsigned<T>::value &&
+                    std::is_integral<RT>::value && std::is_signed<RT>::value,
+                    bool
+                > = true
+            >
+            auto operator&&(const All<RT>&) {
+                FiniteSet<T, Container> intersection;
+                auto zero = internal::zero<T>();
+                for (const auto& element : storage) {
+                    if (element >= zero) {
+                        intersection.storage.insert(std::end(intersection.storage), element);
+                    }
+                }
+                return intersection;
+            }
+
+            // If *this is a subset of the reals and all<RT>() contains the naturals and zero,
+            // then *this && all<RT>() contain all elements of *this that are naturals or zero.
+            template<
+                class RT,
+                std::enable_if_t<
+                    std::is_floating_point<T>::value &&
+                    std::is_signed<RT>::value,
+                    bool
+                > = true
+            >
+            auto operator&&(const All<RT>&) {
+                FiniteSet<T, Container> intersection;
+                auto zero = internal::zero<T>();
+                for (const auto& element : storage) {
+                    if (element >= zero && static_cast<T>(static_cast<unsigned long long>(element)) == element) {
+                        intersection.storage.insert(std::end(intersection.storage), element);
+                    }
+                }
+                return intersection;
+            }
+
+            // If *this is a subset of the reals and all<RT>() contains the integers,
+            // then *this && all<RT>() contain all elements of *this that are integers.
+            template<
+                class RT,
+                std::enable_if_t<
+                    std::is_floating_point<T>::value &&
+                    std::is_integral<RT>::value && std::is_signed<RT>::value,
+                    bool
+                > = true
+            >
+            auto operator&&(const All<RT>&) {
+                FiniteSet<T, Container> intersection;
+                for (const auto& element : storage) {
+                    if (static_cast<T>(static_cast<long long>(element)) == element) {
+                        intersection.storage.insert(std::end(intersection.storage), element);
+                    }
+                }
+                return intersection;
+            }
+
+            // Elements of *this that do not change when cast to RT and back to
+            // T surely belong in *this && all<RT>().
+            template<
+                class RT,
+                std::enable_if_t<
+                    std::is_convertible<T, RT>::value &&
+                    std::is_convertible<RT, T>::value &&
+                    !(std::is_arithmetic<T>::value && std::is_arithmetic<RT>::value),
+                    bool
+                > = true
+            >
+            auto operator&&(const All<RT>&) {
+                FiniteSet<T, Container> intersection;
+                for (const auto& element : storage) {
+                    if (static_cast<T>(static_cast<RT>(element)) == element) {
+                        intersection.storage.insert(std::end(intersection.storage), element);
+                    }
+                }
+                return intersection;
+            }
+
+            auto operator&&(const All<T>&) {
+                return *this;
+            }
+
+            // If *this is a subset of all<RT>(), then *this || all<RT>() == all<RT>(). Like
+            // for operator&&, we're currently only doing this for arithmetic types.
+            template<
+                class RT,
+                std::enable_if_t<
+                    (std::is_arithmetic<T>::value && std::is_floating_point<RT>::value) ||  // RT are the reals
+                    (
+                        std::is_integral<T>::value && !std::is_signed<T>::value &&          // T and RT are the integers.
+                        std::is_integral<RT>::value && !std::is_signed<RT>::value
+                    ) ||
+                    (std::is_signed<T>::value && std::is_arithmetic<RT>::value),            // T are the naturals and zero.
+                    bool
+                > = true
+            >
+            auto operator||(const All<RT>&) {
+                return all<RT>();
+            }
+
             template<class RT, template<class, class...> class RC>
             auto operator&&(const FiniteSet<RT, RC>& rhs) const {
-                FiniteSet<typename std::common_type<T,RT>::type, Container> intersection;
+                FiniteSet<T, Container> intersection;
                 std::set_intersection(
                     std::cbegin(storage), std::cend(storage),
                     std::cbegin(rhs.storage), std::cend(rhs.storage),
@@ -112,6 +280,7 @@ namespace libp {
         return conditional_set(std::move(lhs), std::move(rhs));
     }
 
-};
+}
+
 #endif
 
