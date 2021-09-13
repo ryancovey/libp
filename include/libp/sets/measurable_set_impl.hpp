78c62411cc8d5ff42ec39bfaf5bd29cbf7c0947a
Ryan Zischke <ryan@melb.zischke.net.au>
2021-09-13 12:20:20 +1000

    Forgot to wrap the contents of measurable_set_impl.hpp in namspace libp { ... }, now fixed.

--- include/libp/sets/measurable_set_impl.hpp
+++ include/libp/sets/measurable_set_impl.hpp
@@ -3,60 +3,64 @@
 
 #include <type_traits>
 
-class MeasurableSetImpl { };
+namespace libp {
 
-template<class T>
-class MeasurableSetCRTP : public MeasurableSetImpl { };
+    class MeasurableSetImpl { };
 
-template<
-    class T,
-    std::enable_if_t<std::is_base_of<MeasurableSetImpl, std::decay_t<T>>::value, bool> = true
->
-auto operator==(const T&, const T&) {
-    return true;
-}
+    template<class T>
+    class MeasurableSetCRTP : public MeasurableSetImpl { };
 
-template<
-    class L,
-    class R,
-    std::enable_if_t<
-        std::is_base_of<MeasurableSetImpl, std::decay_t<L>>::value &&
-        std::is_base_of<MeasurableSetImpl, std::decay_t<R>>::value &&
-        !std::is_same<std::decay_t<L>, std::decay_t<R>>::value,
-        bool
-    > = true
->
-auto operator==(const L&, const R&) {
-    return false;
-}
+    template<
+        class T,
+        std::enable_if_t<std::is_base_of<MeasurableSetImpl, std::decay_t<T>>::value, bool> = true
+    >
+    auto operator==(const T&, const T&) {
+        return true;
+    }
 
-template<
-    class L,
-    class R,
-    std::enable_if_t<
-        std::is_base_of<MeasurableSetImpl, std::decay_t<L>>::value &&
-        std::is_base_of<MeasurableSetImpl, std::decay_t<R>>::value,
-        bool
-    > = true
->
-bool operator!=(const L& lhs, const R& rhs) {
-    return !(lhs == rhs);
-}
+    template<
+        class L,
+        class R,
+        std::enable_if_t<
+            std::is_base_of<MeasurableSetImpl, std::decay_t<L>>::value &&
+            std::is_base_of<MeasurableSetImpl, std::decay_t<R>>::value &&
+            !std::is_same<std::decay_t<L>, std::decay_t<R>>::value,
+            bool
+        > = true
+    >
+    auto operator==(const L&, const R&) {
+        return false;
+    }
 
-template<
-    class T,
-    std::enable_if_t<std::is_base_of<MeasurableSetImpl, std::decay_t<T>>::value, bool> = true
->
-auto operator&&(const T& lhs, const T&) {
-    return lhs;
-}
+    template<
+        class L,
+        class R,
+        std::enable_if_t<
+            std::is_base_of<MeasurableSetImpl, std::decay_t<L>>::value &&
+            std::is_base_of<MeasurableSetImpl, std::decay_t<R>>::value,
+            bool
+        > = true
+    >
+    bool operator!=(const L& lhs, const R& rhs) {
+        return !(lhs == rhs);
+    }
 
-template<
-    class T,
-    std::enable_if_t<std::is_base_of<MeasurableSetImpl, std::decay_t<T>>::value, bool> = true
->
-auto operator||(const T& lhs, const T&) {
-    return lhs;
+    template<
+        class T,
+        std::enable_if_t<std::is_base_of<MeasurableSetImpl, std::decay_t<T>>::value, bool> = true
+    >
+    auto operator&&(const T& lhs, const T&) {
+        return lhs;
+    }
+
+    template<
+        class T,
+        std::enable_if_t<std::is_base_of<MeasurableSetImpl, std::decay_t<T>>::value, bool> = true
+    >
+    auto operator||(const T& lhs, const T&) {
+        return lhs;
+    }
+
 }
 
 #endif
